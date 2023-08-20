a00c72e7fade7c21b3362d006ed0e954159cbff6
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2023-08-20 18:41:17 +0800

    Refactor

--- index.js
+++ index.js
@@ -3,22 +3,37 @@ import FormData from 'form-data';
 import fetch from 'node-fetch';
 import fs from 'fs';
 
-try {
-  const apiToken = core.getInput('api_token', { required: true });
-  const projectId = core.getInput('project_id', { required: true });
-  const changelog = core.getInput('changelog', { required: true });
-  const changelogType = core.getInput('changelog_type');
-  const displayName = core.getInput('display_name');
-  const parentFileID = core.getInput('parent_file_id');
-  const gameVersions = core.getInput('game_versions');
-  const releaseType = core.getInput('release_type');
-  const projectRelations = core.getInput('project_relations');
-  const filePath = core.getInput('file_path', { required: true });
-  const debug = core.getBooleanInput('debug');
+const apiToken = core.getInput('api_token', { required: true });
+const projectId = core.getInput('project_id', { required: true });
+const changelog = core.getInput('changelog');
+const changelogType = core.getInput('changelog_type');
+const displayName = core.getInput('display_name');
+const parentFileID = core.getInput('parent_file_id');
+const gameVersions = core.getInput('game_versions');
+const releaseType = core.getInput('release_type');
+const projectRelations = core.getInput('project_relations');
+const filePath = core.getInput('file_path', { required: true });
+const debug = core.getBooleanInput('debug');
 
+try {
+  console.log(`Uploading ${filePath} to project ${projectId}...`);
+  const metadata = parseMetadata();
+  if (debug) {
+    console.log(JSON.stringify(metadata, null, 2));
+  }
+  uploadFile(metadata);
+} catch (error) {
+  core.setFailed(error.message);
+}
+
+function parseMetadata() {
+  if (!parentFileID && !gameVersions) {
+    core.setFailed('You must specify either parent_file_id or game_versions');
+    process.exit(1);
+  }
   if (parentFileID && gameVersions) {
     core.setFailed('You cannot specify both parent_file_id and game_versions');
-    return;
+    process.exit(1);
   }
 
   const metadata = {
@@ -36,15 +51,14 @@ try {
   // remove undefined values
   Object.keys(metadata).forEach(key => metadata[key] === '' && delete metadata[key]);
 
-  if (debug) {
-    core.info(`Uploading ${filePath} to project ${projectId}...`);
-    core.info(JSON.stringify(metadata));
-  }
+  return metadata;
+}
 
+function uploadFile(metadata) {
   const form = new FormData();
   form.append('file', fs.createReadStream(filePath));
   form.append('metadata', JSON.stringify(metadata));
-  
+
   fetch(`https://dev.bukkit.org/api/projects/${projectId}/upload-file`, {
     method: 'POST',
     headers: {
@@ -54,17 +68,13 @@ try {
     },
     body: form
   })
-  .then(res => {
-    if (!res.ok) {
-      if (debug) {
-        console.log(res);
-        core.info(JSON.stringify(res.json()));
+    .then(async res => {
+      if (!res.ok) {
+        if (debug) {
+          console.log(await res.text());
+          console.log(JSON.stringify(await res.json(), null, 2));
+        }
+        core.setFailed(`Request failed with status code ${res.status}`);
       }
-      core.setFailed(`Request failed with status code ${res.status}`);
-      return;
-    }
-    core.setOutput('success', res.json())
-  })
-} catch (error) {
-  core.setFailed(error.message);
+    })
 }
